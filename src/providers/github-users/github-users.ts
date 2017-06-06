161037fc52365f2735d3f5ac90ad0123bab3016b
Lucas Assmann <l.assmann.30@gmail.com>
2017-06-06 20:05:19 -0300

    added a provider github

---
+++ src/providers/github-users/github-users.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { User } from '../../models/user';
+
+@Injectable()
+export class GithubUsers {
+  githubApiUrl = 'https://api.github.com';
+
+  constructor(public http: Http) { }
+
+  // Load all github users
+  load(): Observable<User[]> {
+    return this.http.get(`${this.githubApiUrl}/users`)
+      .map(res => <User[]>res.json());
+  }
+}
